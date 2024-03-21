158080db6d0e5d5f63e31fb8d37abf4923c913ed
Brodie <2915903s@student.gla.ac.uk>
2024-03-21 13:49:58 +0000

    added first models.py, migrated and created admin superuser and tested adding and deleting using admin interface

--- pets_for_students/admin.py
+++ pets_for_students/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from pets_for_students.models import Students, Cats
 
 # Register your models here.
+
+
+admin.site.register(Students)
+admin.site.register(Cats)
--- pets_for_students/models.py
+++ pets_for_students/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+    firstname = models.CharField(max_length=128)
+    lastname = models.CharField(max_length=128)
+    def __str__(self):
+        return self.firstname
+
+class Cats(models.Model):
+    student = models.ForeignKey(Students, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
