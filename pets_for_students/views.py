af6745e5b0d2d3ea02a24afaae8051be4623e382
Brodie <2915903s@student.gla.ac.uk>
2024-03-21 14:40:02 +0000

    added cat and student lists to templates and they are correctly formatted on the pages

--- pets_for_students/views.py
+++ pets_for_students/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from pets_for_students.models import Cats, Students
+
 
 
 
@@ -6,7 +8,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'pets_for_students/index.html')
+    cats_list = Cats.objects.order_by('name')
+    students_list = Students.objects.order_by('lastname')
+    context_dict = {}
+    context_dict['cats'] = cats_list
+    context_dict['students'] = students_list
+
+    return render(request, 'pets_for_students/index.html', context=context_dict )
 
 def pets(request):
-    return render(request, 'pets_for_students/pets.html')
+    cats_list = Cats.objects.order_by('-name')
+    context_dict = {}
+    context_dict['cats'] = cats_list
+
+    return render(request, 'pets_for_students/pets.html', context=context_dict)
