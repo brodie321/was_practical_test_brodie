cb69bc4c0607be5d0bcd441cdcdd13e838c45e14
Brodie <2915903s@student.gla.ac.uk>
2024-03-21 23:24:45 +0000

    added counting number of cats

--- pets_for_students/models.py
+++ pets_for_students/models.py
@@ -5,12 +5,18 @@ from django.db import models
 class Students(models.Model):
     firstname = models.CharField(max_length=128)
     lastname = models.CharField(max_length=128)
+    noOfCats = models.IntegerField(default=0)
     def __str__(self):
         return self.firstname
 
 class Cats(models.Model):
     student = models.ForeignKey(Students, on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
+
+    def save(self, *args, **kwargs):
+        self.student.noOfCats = self.student.cats_set.count() + 1
+        self.student.save()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
